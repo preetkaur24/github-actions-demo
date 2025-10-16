a3a395c23958fe8d0fcdf9f78bc50ef09607f5f2
Preet Kaur <preet.kaur@slu.edu>
2025-10-16 16:18:46 -0500

    Force remove all trailing newlines

--- app.py
+++ app.py
@@ -1,7 +1,1 @@
-def add(a, b):
-    print(f"Adding {a} and {b}")
-    return a + b
-
-
-if __name__ == "__main__":
-    print(add(2, 3))
+ÿş
--- test_app.py
+++ test_app.py
@@ -1,9 +1,1 @@
-from app import add
-
-
-def test_add():
-    assert add(2, 3) == 5
-
-
-def test_add_negative():
-    assert add(-2, -3) == -5
+ÿş
